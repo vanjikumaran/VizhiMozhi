aff21c989744dd5e4fb40d9b943c8c20ce450b08
Vanjikumaran <vanjikumaran@gmail.com>
2015-03-20 14:21:03 +0530

    Update Program.cs

--- SrilankanTamilFingerSpelling/SrilankanTamilFingerSpelling/Program.cs
+++ SrilankanTamilFingerSpelling/SrilankanTamilFingerSpelling/Program.cs
@@ -15,32 +15,13 @@ namespace SrilankanTamilFingerSpelling
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-
-
-
-            //Application.Run(new GUI.frmWebCamGUI());
-            //Application.Run(new GUI.frmTrainingSession());
-            //     Application.Run(new GUI.frmRegognition());
-            //  Application.Run(new GUI.frmMDIParent());
             Application.Run(new GUI.frmSplashScreen());
-
-
-           // Application.Run(new GUI.frmMDIParent());
-
-         Application.Run(new GUI.frmMainInterFace());
+            Application.Run(new GUI.frmMainInterFace());
         }
-
-
-
 
         private static void ShowAssemblies(object sender, AssemblyLoadEventArgs e)
         {
             // Store name of assembly in the queue
-
-   
         }
-
-
     }
 }
