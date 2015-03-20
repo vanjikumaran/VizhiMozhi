b69aa4aefb91c2d7e1fdcfc3bdd6cb48a5cabe57
Vanjikumaran <vanjikumaran@gmail.com>
2015-03-20 14:28:58 +0530

    Update WordCreator.cs

--- SrilankanTamilFingerSpelling/SrilankanTamilFingerSpelling/Utilities/WordCreator.cs
+++ SrilankanTamilFingerSpelling/SrilankanTamilFingerSpelling/Utilities/WordCreator.cs
@@ -7,54 +7,16 @@ namespace SrilankanTamilFingerSpelling.Utilities
 {
     class WordCreator
     {
-
         private String letterBase;
         private String letterNew;
-
-
-
-
-
-
-
         public string processLetters(String letter)
         {
-
             if (letterBase == null)
             {
                 // create initial Base Letter
                this.letterBase = letter;
                return 0;
-
             }
-
-
-            else
-            {
-                if (true)
-                {
-                    
-                }
-            }
-
-
-
-
-        
-        
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
